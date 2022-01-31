dd339e51fdbd9301bf998a0ee763b5ecbf9648ba
kyoung-jnn <rudwls468@gmail.com>
2022-01-31 21:47:14 +0900

    env: styled-components 설정

---
+++ styles/global-styles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%; // 1rem === 10px
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  * { font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;}
+`;
+
+export default GlobalStyle;
