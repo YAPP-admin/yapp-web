10426d3598480009dd3afb67d1caac2d6a9aeb9f
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-01 23:34:59 +0900

    feat: Design System 초기 구성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,10 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/global-styles';
 import theme from 'styles/theme';
-import media from 'styles/media';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={{ ...theme, ...media }}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,72 @@
-const theme = {
-  white: '#ffffff',
+import { css, DefaultTheme } from 'styled-components';
+
+/* Based on YAPP Web Design System */
+
+const palette = {
+  white: '#FFFFFF',
   black: '#000000',
+  gray: '#A6A6A6',
+  lightGray: '#D3D3D3',
+};
+
+const fontSize = {
+  xxxSmall: '1.5rem',
+  xxSmall: '1.6rem',
+  xSmall: '1.8rem',
+  small: '2.4rem',
+  medium: '2.8rem',
+  large: '3.0rem',
+  xLarge: '3.6rem',
+  xxLarge: '4.2rem',
+  xxxLarge: '4.8rem',
+  xxxxLarge: '6.0rem',
+};
+
+const fontWeight = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+};
+
+const lineHeight = {
+  xSmall: '120%',
+  small: '130%',
+  medium: '140%',
+  large: '150%',
+  xLarge: '160%',
+};
+
+const textStyle = {
+  mobile: {
+    Button_Point: css`
+      font-size: ${fontSize.xSmall};
+      font-weight: ${fontWeight.medium};
+      line-height: ${lineHeight.xSmall};
+    `,
+    Button: css`
+      font-size: ${fontSize.xxSmall};
+      font-weight: ${fontWeight.medium};
+      line-height: ${lineHeight.xSmall};
+    `,
+  },
+  web: {
+    Button_Point: css`
+      font-size: ${fontSize.small};
+      font-weight: ${fontWeight.semibold};
+      line-height: ${lineHeight.xSmall};
+    `,
+    Button: css`
+      font-size: ${fontSize.small};
+      font-weight: ${fontWeight.medium};
+      line-height: ${lineHeight.xSmall};
+    `,
+  },
+};
+
+const theme: DefaultTheme = {
+  palette,
+  textStyle,
 };
 
 export default theme;
