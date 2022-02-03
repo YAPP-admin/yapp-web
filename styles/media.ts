fd8437cc948b0f4b557ec970dd8984e1307c2f95
이도현 <ksmfou98@naver.com>
2022-02-03 20:31:58 +0900

    feat: 화면 size 상수화

--- styles/media.ts
+++ styles/media.ts
@@ -1,11 +1,15 @@
+import Breakpoints from 'constants/breakpoints';
 import { Media } from 'styled-components';
 
 const mediaQuery = (maxWidth: number) => `@media (max-width: ${maxWidth}px)`;
 
+// @Todo: 화면 사이즈 모두 나오면 상수화 예정
+const { xxlarge } = Breakpoints;
+
 const media: Media = {
   mobile: mediaQuery(375),
   tablet: mediaQuery(834),
-  desktop: mediaQuery(1920),
+  desktop: mediaQuery(xxlarge),
   custom: mediaQuery,
 };
 
