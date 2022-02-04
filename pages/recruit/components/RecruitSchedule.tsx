bc2e7c3ed402a7ea9ecf04443071ff1e87b00abd
이도현 <ksmfou98@naver.com>
2022-02-04 23:55:33 +0900

    feat: 모집 일정 컴포넌트 생성

--- pages/recruit/components/RecruitSchedule.tsx
+++ pages/recruit/components/RecruitSchedule.tsx
@@ -1,7 +1,24 @@
+import Breakpoints from 'constants/breakpoints';
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 
 function RecruitSchedule(): ReactElement {
-  return <div>RecruitSchedule</div>;
+  return (
+    <ScheduleBlock>
+      <ScheduleInner>asd</ScheduleInner>
+    </ScheduleBlock>
+  );
 }
+
+const ScheduleBlock = styled.div`
+  width: 100%;
+`;
+
+const ScheduleInner = styled.div`
+  width: ${Breakpoints.xlarge}px;
+  padding: 100px 0;
+  margin: 0 auto;
+  background-color: lightblue;
+`;
 
 export default RecruitSchedule;
