1aa19cf84d3dd1cfb3975dd31776a11b1abf8585
이도현 <ksmfou98@naver.com>
2022-02-04 23:56:21 +0900

    feat: 지원 방법 컴포넌트 생성

--- pages/recruit/components/ApplyWay.tsx
+++ pages/recruit/components/ApplyWay.tsx
@@ -1,7 +1,53 @@
+import { Box } from 'components';
+import Breakpoints from 'constants/breakpoints';
+import { RECRUIT_APPLY_WAY } from 'constants/recruit';
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+import SectionTitle from './SectionTitle';
 
 function ApplyWay(): ReactElement {
-  return <div>ApplyWay</div>;
+  const { title, ways } = RECRUIT_APPLY_WAY;
+
+  return (
+    <ApplyWayBlock>
+      <ApplyWayInner>
+        <SectionTitle title={title} />
+        <SectionContent>
+          {ways.map((way) => (
+            <WayBox width={380} height={307} backgroundColor="lightGray">
+              <WaySubTitle>{way.title}</WaySubTitle>
+            </WayBox>
+          ))}
+        </SectionContent>
+      </ApplyWayInner>
+    </ApplyWayBlock>
+  );
 }
+
+const ApplyWayBlock = styled.div`
+  width: 100%;
+`;
+
+const ApplyWayInner = styled.div`
+  width: ${Breakpoints.xlarge}px;
+  margin: 0 auto;
+  padding: 100px 0;
+`;
+
+const SectionContent = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const WayBox = styled(Box)`
+  padding: 32px;
+`;
+
+const WaySubTitle = styled.div`
+  ${({ theme }) => theme.textStyle.web.SubTitle}
+`;
+
+const WayContent = styled.div``;
 
 export default ApplyWay;
