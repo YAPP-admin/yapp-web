bfdff7b6175e0211b019d783dc42866e7333bd35
이도현 <ksmfou98@naver.com>
2022-02-04 20:59:46 +0900

    feat: Recruit 페이지 생성

---
+++ pages/recruit/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+function RecruitPage(): ReactElement {
+  return <Wrapper>RecruitPage</Wrapper>;
+}
+
+const Wrapper = styled.div``;
+
+export default RecruitPage;
