52b1df276b86fae39dd4217694d31f32541423ac
이도현 <ksmfou98@naver.com>
2022-02-04 21:00:25 +0900

    feat: FAQ 컴포넌트 생성

---
+++ pages/recruit/components/FrequentlyAskedQuestions.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+function FrequentlyAskedQuestions(): ReactElement {
+  return <div>FrequentlyAskedQuestions</div>;
+}
+
+export default FrequentlyAskedQuestions;
