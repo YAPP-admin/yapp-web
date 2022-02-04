624537842964001f2d33254aa08ecfa68be610e5
이도현 <ksmfou98@naver.com>
2022-02-04 23:56:55 +0900

    feat: 채용 관련 상수 설정

---
+++ constants/recruit.ts
@@ -0,0 +1,47 @@
+/**
+ * 작성 Tip
+ * - 줄바꿈은 <br /> 태그를 넣어주세요 !
+ * - <span class="test"> 테스트 </span> 과 같은 html 태그와 class 를 넣어서 사용할 수 있습니다!
+ * - html 태그 사용시 `` 백틱 으로 문자열을 감싸주세요!
+ */
+
+export const RECRUIT_BANNER = {
+  title: '지금은 모집기간이 아닙니다',
+  description: '현재 19기 활동 중! 다음 기수는 2022.03에 예정되어 있습니다',
+  buttonName: '모집 알림 신청',
+};
+
+export const RECRUIT_BANNER_ACTIVE = {
+  title: 'YAPP 20기 모집',
+  description: `YAPP 20기에서 6개월간 활동할 기획자(PM)/디자이너/개발자 신입 회원을 모집합니다.<br /> 
+    IT 분야에 대한 열정과 의지가 넘쳐나고, 동아리에서 많은 사람들과 즐겁게 활동하고 싶은 분들의 많은 지원 바랍니다!`,
+  buttonName: '지원하기',
+};
+
+export const RECRUIT_APPLY_WAY = {
+  title: '지원 방법',
+  ways: [
+    {
+      title: '모집기간',
+      description: `YAPP에서는 1년에 두번<br />
+                    (6-7월/1-2월)<br />
+                    열정을 가지고 활동에 함께할<br />
+                    YAPPian을 모집합니다`,
+    },
+    {
+      title: '지원자격',
+      description: `6개월 동안 활동하는데<br />
+                    문제가 없고,<br />
+                    실력을 키우기 위해 배우려는<br />
+                    의지가 있는 누구나<br />
+                    <span class="small">대학생이 아닌 졸업생, 직장인도 가능합니다.</span>`,
+    },
+    {
+      title: '지원방법',
+      description: `1차 서류<br />
+                    <span class="small">모집 페이지에서 지원서 작성<br />
+                    및 포트폴리오 제출</span><br />
+                    2차 면접 후 최종 합격`,
+    },
+  ],
+};
