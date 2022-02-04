94eb24fd268e7a0572a843037127a06a5402acb7
이도현 <ksmfou98@naver.com>
2022-02-04 23:56:02 +0900

    feat: Qna 컴포넌트 생성

--- pages/recruit/components/FrequentlyAskedQuestions.tsx
+++ pages/recruit/components/FrequentlyAskedQuestions.tsx
@@ -1,7 +1,24 @@
+import Breakpoints from 'constants/breakpoints';
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 
 function FrequentlyAskedQuestions(): ReactElement {
-  return <div>FrequentlyAskedQuestions</div>;
+  return (
+    <FAQBlock>
+      <FAQInner>asd</FAQInner>
+    </FAQBlock>
+  );
 }
+
+const FAQBlock = styled.div`
+  width: 100%;
+`;
+
+const FAQInner = styled.div`
+  width: ${Breakpoints.xlarge}px;
+  margin: 0 auto;
+  padding: 100px 0;
+  background-color: lightcoral;
+`;
 
 export default FrequentlyAskedQuestions;
