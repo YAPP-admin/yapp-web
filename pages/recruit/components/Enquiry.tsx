886beed8f07e773a528a7cefc2fcf3f425409a27
이도현 <ksmfou98@naver.com>
2022-02-04 23:56:11 +0900

    feat: 문의 사항 컴포넌트 생성

--- pages/recruit/components/Enquiry.tsx
+++ pages/recruit/components/Enquiry.tsx
@@ -1,7 +1,24 @@
+import Breakpoints from 'constants/breakpoints';
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 
 function Enquiry(): ReactElement {
-  return <div>Enquiry</div>;
+  return (
+    <EnquiryBlock>
+      <EnquiryInner>asd</EnquiryInner>
+    </EnquiryBlock>
+  );
 }
+
+const EnquiryBlock = styled.div`
+  width: 100%;
+`;
+
+const EnquiryInner = styled.div`
+  width: ${Breakpoints.xlarge}px;
+  margin: 0 auto;
+  padding: 100px 0;
+  background-color: lightcyan;
+`;
 
 export default Enquiry;
