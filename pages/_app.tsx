268479c5b1d469354a3814dfa1b358219e7deb48
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-08 18:58:25 +0900

    feat: SEO 추가 및 기본 레이아웃 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { ThemeProvider } from 'styled-components';
+import { LayoutWrapper } from 'components';
 import GlobalStyle from 'styles/global-styles';
 import theme from 'styles/theme';
+import Main from 'components/home/Main';
+import { SEO, Header, Footer } from 'components';
 
 function App({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
+
   return (
     <ThemeProvider theme={theme}>
+      <SEO />
       <GlobalStyle />
+      {asPath === '/' && <Main />}
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
