78bd9750e01b92133708c42012ad87d113928003
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-09 18:35:26 +0900

    feat: Carousel 컴포넌트 구현

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -1,46 +1,34 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef, useCallback } from 'react';
 import styled, { css } from 'styled-components';
 import Slider, { Settings } from 'react-slick';
 import media from 'styles/media';
 
 interface CarouselProps {
-  data: any;
+  data: any; // 데이터 확정 전, any 타입
 }
 
 function Carousel({ data }: CarouselProps) {
+  const slickRef = useRef<Slider>(null);
   const settings = useMemo<Settings>(
     () => ({
       dots: true, // 캐러셀 하단부 점으로 한번에 이동
       arrows: true, // 캐러셀을 움직이는 화살표 추가
-      centerMode: true,
-      centerPadding: '100px',
+      centerMode: true, // 중앙 정렬
+      centerPadding: '220px', // 좌 우 카드가 padding 만큼 삐져나오게 함
       slidesToShow: 3, // 한번에 보여줄 슬라이드 수
-      infinite: true, // 무한 캐러셀 여부
-      arrow: true,
-      autoplay: true, // 자동재생 여부
+      infinite: true, // 무한 루프
+      arrow: false, // 좌 우 화살표
+      autoplay: true, // 자동재생
       speed: 400, // 슬라이더 속도
-      pauseOnHover: true,
-      draggable: true,
-      nextArrow: <Arrow></Arrow>,
-      prevArrow: <Arrow></Arrow>,
+      pauseOnHover: true, // Hover시 멈춤
+      draggable: true, // 드래그 가능
+      // 반응형, 현재는 임시 구현
       responsive: [
         {
-          breakpoint: 1200,
-          settings: {
-            centerPadding: '50px',
-          },
-        },
-        {
-          breakpoint: 800,
+          breakpoint: 375, // 추후 breakpoint 상수로 대체
           settings: {
             slidesToShow: 1,
-            centerPadding: '200px',
-          },
-        },
-        {
-          breakpoint: 400,
-          settings: {
-            slidesToShow: 1,
+            centerPadding: '0',
           },
         },
       ],
@@ -48,28 +36,45 @@ function Carousel({ data }: CarouselProps) {
     [],
   );
 
+  const handlePrevious = useCallback(() => slickRef.current!.slickPrev(), []);
+  const handleNext = useCallback(() => slickRef.current!.slickNext(), []);
+
   return (
     <CarouselContainer>
-      <Slider {...settings}>
+      <Slider ref={slickRef} {...settings}>
         {data.map(({ img }: any, idx: any) => (
           <ProjectCard className="project-card" key={idx}>
             {img}
           </ProjectCard>
         ))}
       </Slider>
+      <>
+        <Arrow left onClick={handlePrevious}></Arrow>
+        <Arrow onClick={handleNext}></Arrow>
+      </>
     </CarouselContainer>
   );
 }
 
 const CarouselContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   width: 100%;
-  margin: 64px 0;
+  min-width: 1920px;
+  margin: 10px 0 64px;
   overflow: hidden;
 
-  .slick-track {
-    height: 331px;
+  // Carousel Container
+
+  .slick-slide {
+    height: 400px;
   }
 
+  .slick-list .slick-track {
+    height: 331px;
+  }
   .slick-slide {
     display: flex;
     justify-content: center;
@@ -77,43 +82,32 @@ const CarouselContainer = styled.div`
     transition: transform 1s;
   }
 
-  // 슬라이더 중앙 요소
-  .slick-center.slick-active .project-card {
+  // 중앙 요소
+  .slick-center.slick-active {
     transform: scale(1.3);
 
-    background-color: #c4c4c4;
-  }
-
-  // 좌 우 버튼
-  .slick-arrow {
-    position: absolute;
-    top: 50%;
-    border: none;
-    z-index: 300;
-  }
-
-  .slick-prev {
-    left: 40%;
-  }
-
-  .slick-next {
-    right: 40%;
+    .project-card {
+      background-color: #c4c4c4;
+    }
   }
 
   // Dots
   .slick-dots {
+    padding-bottom: 30px;
+    button::before {
+      color: #111111;
+    }
+
     .slick-active {
       button::before {
-        color: #c1c1c1;
+        color: #000000;
       }
-    }
-    button::before {
-      color: #e9e9e9;
     }
   }
 `;
 
 const ProjectCard = styled.div`
+  display: block;
   border-radius: 20px;
   background-color: #e8e8e8;
   cursor: pointer;
@@ -127,25 +121,24 @@ const ProjectCard = styled.div`
   }
 `;
 
-const Arrow = styled.div`
-  background: 'red';
+const Arrow = styled.button<{ left?: boolean }>`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
   width: 32px;
   height: 32px;
-`;
+  border-radius: 16px;
+  background-color: #e8e8e8;
 
-const Dots = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 40px;
-`;
-
-const Dot = styled.button<{ active?: boolean }>`
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  margin: 0 16px;
-  background: ${({ active }) => (active ? '#000' : '#e8e8e8')};
-  cursor: pointer;
+  ${({ left }) =>
+    left
+      ? css`
+          left: 40%;
+        `
+      : css`
+          right: 40%;
+        `};
 `;
 
 export default Carousel;
