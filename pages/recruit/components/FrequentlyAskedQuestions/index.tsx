a6224ec27a17bb2856fd8f4ecea181b8aa79bb09
이도현 <ksmfou98@naver.com>
2022-02-24 22:03:23 +0900

    Feat: FAQ 반응형 구현

--- pages/recruit/components/FrequentlyAskedQuestions/index.tsx
+++ pages/recruit/components/FrequentlyAskedQuestions/index.tsx
@@ -3,6 +3,7 @@ import { RECRUIT_FAQ } from 'database/recruit';
 import { ArrowDown } from 'public/assets/icons';
 import React, { ReactElement, useState } from 'react';
 import styled from 'styled-components';
+import media from 'styles/media';
 import { SectionTemplate, SectionTitle } from '..';
 
 function FrequentlyAskedQuestions(): ReactElement {
@@ -27,17 +28,19 @@ function FrequentlyAskedQuestions(): ReactElement {
             backgroundColor="lightestGrey"
             key={`faq-${subTitle}`}
           >
-            <FAQSubTitle onClick={() => handleToggleFaq(subTitle)}>
-              <span>{subTitle}</span>
-              <TitleButton isOpen={isOpen}>
-                <ArrowDown />
-              </TitleButton>
-            </FAQSubTitle>
-            {isOpen && (
-              <FQASubContent
-                dangerouslySetInnerHTML={{ __html: description }}
-              />
-            )}
+            <FAQBoxInner>
+              <FAQSubTitle onClick={() => handleToggleFaq(subTitle)}>
+                <span>{subTitle}</span>
+                <TitleButton isOpen={isOpen}>
+                  <ArrowDown />
+                </TitleButton>
+              </FAQSubTitle>
+              {isOpen && (
+                <FQASubContent
+                  dangerouslySetInnerHTML={{ __html: description }}
+                />
+              )}
+            </FAQBoxInner>
           </FAQBox>
         ))}
       </SectionContent>
@@ -48,15 +51,28 @@ function FrequentlyAskedQuestions(): ReactElement {
 const SectionContent = styled.div`
   display: flex;
   flex-direction: column;
+  ${media.tablet} {
+    padding: 0 22px;
+  }
 `;
 
 const FAQBox = styled(Box)`
-  padding: 32px;
+  padding: 0;
   margin-bottom: 24px;
   min-height: 102px;
   height: auto;
   &:last-child {
     margin-bottom: 0;
+  }
+  ${media.mobile} {
+    min-height: 77px;
+  }
+`;
+
+const FAQBoxInner = styled.div`
+  padding: 32px;
+  ${media.mobile} {
+    padding: 24px;
   }
 `;
 
@@ -66,6 +82,9 @@ const FAQSubTitle = styled.div`
   justify-content: space-between;
   align-items: center;
   cursor: pointer;
+  ${media.mobile} {
+    ${({ theme }) => theme.textStyle.mobile.Body_Point}
+  }
 `;
 
 const TitleButton = styled.button<{ isOpen: boolean }>`
@@ -77,6 +96,12 @@ const FQASubContent = styled.div`
   ${({ theme }) => theme.textStyle.web.Body_1};
   margin-top: 24px;
   width: 1056px;
+  ${media.tablet} {
+    width: 100%;
+  }
+  ${media.mobile} {
+    ${({ theme }) => theme.textStyle.mobile.Body_1};
+  }
 `;
 
 export default FrequentlyAskedQuestions;
