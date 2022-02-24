aab5712c93f517155f956b274cf9c9ff167f2bf2
KyoungJin Roh <kyoung-jnn@adenasoft.com>
2022-02-24 12:12:36 +0900

    feat: 전체 레이아웃 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,17 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { useRouter } from 'next/router';
-import { LayoutWrapper } from 'components';
-import GlobalStyle from 'styles/global-styles';
 import theme from 'styles/theme';
-import IntroSection from 'pages/components/IntroSection';
-import { SEO, Header, Footer } from 'components';
-import PATH from 'constants/path';
-import FloatingButton from 'components/FloatingButton';
+import { SEO, LayoutWrapper } from 'components';
+import GlobalStyle from 'styles/global-styles';
 
 function App({ Component, pageProps }: AppProps) {
-  const { asPath } = useRouter();
-
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <SEO />
-        {/* 추후 LayoutWrapper 안에 넣을 예정 */}
-        {asPath === PATH.Home && <LandingMain />}
-
         <LayoutWrapper>
           <Component {...pageProps} />
-          <FloatingButton />
         </LayoutWrapper>
       </ThemeProvider>
     </>
