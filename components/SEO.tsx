7e54535bdbb2a25cc3058f5fc9a6f508bd9f07c2
이도현 <ksmfou98@naver.com>
2022-02-28 21:13:15 +0900

    feat: favicon, meta title 수정

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -4,10 +4,9 @@ const SEO = () => {
   return (
     <Head>
       {/* SEO 용 */}
-      <title>YAPP</title>
+      <title>YAPP 공식홈페이지</title>
       <meta name="description" content="YAPP 공식사이트" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
     </Head>
   );
 };
