9915141f15cffa44e9870b4f9316f8df9e4bac60
kyoung-jnn <rudwls468@gmail.com>
2022-03-05 23:45:01 +0900

    feat: 타입 추가

--- constants/yapp.ts
+++ constants/yapp.ts
@@ -3,6 +3,8 @@ const Yapp = {
   YAPP_GENERATION: '20' as const, // 기수
   YAPP_OFFICIAL_EMAIL: 'support@yapp.co.kr' as const,
   YAPP_FACEBOOK: 'https://ko-kr.facebook.com/yapp.co.kr/' as const,
-};
+  YAPP_KAKAO: 'http://pf.kakao.com/_aGxofd',
+  YAPP_MAIL: 'support@yapp.co.kr',
+} as const;
 
 export default Yapp;
--- database/home.ts
+++ database/home.ts
@@ -10,7 +10,7 @@ export const CURRENT_INFO_DATA = [
   },
   {
     title: '현재 활동 회원 🏃🏻‍♀️',
-    content: '50원',
+    content: '50명',
   },
   {
     title: '누적 활동 인원 👩🏻',
@@ -60,31 +60,40 @@ export const CAROUSEL_DATA = [
   },
 ];
 
-/** Facebook News Card */
+/** Social News Card */
+// 태그 형식 가능합니다.
 export const NEWS_DATA = [
   {
-    image: '/assets/images/facebook1.png',
+    image: '/assets/images/social1.png',
     link: 'https://ko-kr.facebook.com/yapp.co.kr/',
-    content:
-      '얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북',
+    content: `안녕하세요! YAPP 20기 운영진입니다 🤗
+    드디어 3월 6일 오늘부터, 3월 17일(목) 오후 11시 59분까지
+    YAPP의 20기로 함께할 신규 회원을 모집합니다!
+    YAPP에서 열정과 아이디어 충만한 기획자, 사용자 중심의 UI와 UX를 설계하는 디자이너, 직접 새로운 서비스를 런칭해보...`,
   },
   {
-    image: '/assets/images/facebook2.png',
-    link: 'https://ko-kr.facebook.com/yapp.co.kr/',
-    content:
-      '얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북',
+    image: '/assets/images/social2.png',
+    link: 'https://www.facebook.com/150172688390434/posts/7034770019930632/?d=n',
+    content: `📢 YAPP 신입 20기 모집 D-1
+    안녕하세요. IT 연합동아리 YAPP 20기 운영진입니다 :)
+    19기 활동이 끝나고 많은 분들이 20기 모집 일정에 대해 문의 주셨는데요, 드디어 YAPP 20기 모집일이 다가왔습니다!
+    자세한 모집 내용과 일정은 내일 올라오는 공지를 통해 확인...`,
   },
   {
-    image: '/assets/images/facebook3.png',
-    link: 'https://ko-kr.facebook.com/yapp.co.kr/',
-    content:
-      '얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북 포스팅 내용이 들어갑니다. 얍 페이스북',
+    image: '/assets/images/social3.png',
+    link: 'https://www.facebook.com/150172688390434/posts/7010824668991834/?d=n',
+    content: `안녕하세요. 기업형 IT 연합동아리 YAPP 입니다!
+    약 6개월동안 진행되었던 19기 활동이 끝났는데요! 모든 팀원들이 열심히 활동해주신 덕분에 대다수의 팀들이 런칭을 완료할 수 있었습니다 ✨
+    그 중 “도토리함” 을 개발한 Web 2팀 중 Frontend 개발자 ...`,
   },
 ];
 
 /** Sponsor 이미지 경로 */
 export const SPONSOR_DATA = [
-  /* {
-    image: '이미지 경로',
-  }, */
+  {
+    image: '/assets/images/sponsor1.png',
+  },
+  {
+    image: '/assets/images/sponsor2.png',
+  },
 ];
