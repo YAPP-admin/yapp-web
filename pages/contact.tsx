e5edb2a59b1786240ae3295cf9aa3154e677daf8
이도현 <ksmfou98@naver.com>
2022-03-05 10:51:04 +0900

    feat: contact layout 구현

---
+++ pages/contact.tsx
@@ -0,0 +1,8 @@
+import ContactLayout from 'pages-components/contact/ContactLayout';
+import React, { ReactElement } from 'react';
+
+function ContactPage(): ReactElement {
+  return <ContactLayout>ContactPage</ContactLayout>;
+}
+
+export default ContactPage;
