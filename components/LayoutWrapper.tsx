f433ed1b5c834a4d78144c7ab8d98c0efc5e9812
이도현 <ksmfou98@naver.com>
2022-03-06 20:37:54 +0900

    fix: background color change & button hover

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -38,7 +38,6 @@ const LayoutWrapperStyled = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100%;
-  background-color: ${({ theme }) => theme.palette.grey_100};
 `;
 
 export default LayoutWrapper;
