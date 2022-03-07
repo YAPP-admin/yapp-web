fd2bd5a5646f7a112524339632f0fa047b45bf8d
kyoung-jnn <rudwls468@gmail.com>
2022-03-07 15:04:46 +0900

    feat: SEO 적용

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,25 +1,29 @@
 import Head from 'next/head';
+import siteMetadata from 'database/sitemap';
 
 const SEO = () => {
   return (
     <Head>
-      {/* SEO 용 */}
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/assets/images/favicon.png" />
-
+      {/* Default SEO */}
       <title>YAPP</title>
-      <meta
-        name="description"
-        content="작은 아이디어로 세상을 크게 변화시키는 IT동아리, YAPP"
-      />
+      <link rel="icon" href="/assets/images/favicon.png" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content={siteMetadata.description} />
+      <meta name="keywords" content={siteMetadata.keywords} />
+      <meta name="author" content={siteMetadata.author} />
+      <meta name="reply-to" content={siteMetadata.email} />
+
+      {/* 로봇 방문 허용 */}
+      <meta name="robots" content="follow, index" />
 
       {/* Open Graph */}
-      <meta property="og:title" content="YAPP" />
-      <meta
-        property="og:description"
-        content="작은 아이디어로 세상을 크게 변화시키는 IT동아리, YAPP"
-      />
-      <meta property="og:image" content="https://i.ibb.co/3MJkrs0/image.png" />
+      <meta property="og:site_name" content={siteMetadata.sitename} />
+      <meta property="og:title" content={siteMetadata.title} />
+      <meta property="og:description" content={siteMetadata.description} />
+      <meta property="og:image" content={siteMetadata.image} />
+      <meta property="og:url" content={siteMetadata.siteUrl} />
+      <meta property="og:locale" content={siteMetadata.locale} />
+      <meta property="og:type" content={siteMetadata.type} />
     </Head>
   );
 };
