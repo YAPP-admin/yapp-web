086df07a6334f59a47143ba61c8907a833b8fa5b
kyoung-jnn <rudwls468@gmail.com>
2022-03-28 16:31:08 +0900

    feat: GA 구현 방식 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,35 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
-import theme from 'styles/theme';
 import { SEO, LayoutWrapper } from 'components';
 import GlobalStyle from 'styles/global-styles';
+import theme from 'styles/theme';
+import * as ga from 'utils/gtag';
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // 페이지 별 GA 적용
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
         <SEO />
         <LayoutWrapper>
           <Component {...pageProps} />
