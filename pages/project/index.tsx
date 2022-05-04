4ad9c2e56af5f032d37b69b42173eff4a8a1bd8c
kyoung-jnn <rudwls468@gmail.com>
2022-05-05 01:31:44 +0900

    feat: 프로젝트 페이지 임시 구현

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
-import Link from 'next/link';
-import Image from 'next/image';
-import { getAllProjects } from 'utils/getAllProjects';
 import styled from 'styled-components';
+import { TabMenu, ProjectCard, Button } from 'components/common';
+import { getAllProjects } from 'utils/getAllProjects';
+import { PROJECT_CATEGORIES } from 'database/project';
+import media from 'styles/media';
 
 export const getStaticProps: GetStaticProps = async () => {
   const projects = await getAllProjects();
+
   const projectData = projects.map(({ project, slug }: any) => {
     return {
-      title: project.title,
+      title: project.name,
       thumbnail: project.thumbnail,
+      tags: project.tags,
+      category: project.field,
+      generation: project.generation,
       url: slug.join('/'),
     };
   });
@@ -27,38 +33,117 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
+export type ProjectCategoryTypes = 'iOS' | 'Android' | 'Web' | 'ML';
+
 interface ProjectProps {
   projects: any;
 }
 
 // Project Grid View
 function Project({ projects }: ProjectProps) {
+  const [category, setCategory] = useState<ProjectCategoryTypes>(
+    PROJECT_CATEGORIES[0],
+  );
+
   return (
-    <>
-      {projects.map(({ title, url, thumbnail }: any) => (
-        <Link href={`/project/${url}`} key={title}>
-          <div style={{ cursor: 'pointer', width: '280px' }}>
-            <ThumbnailWrapper>
-              <Image
-                src={thumbnail}
-                alt="project-thumbnail"
-                layout="fill"
-                placeholder="blur"
-                blurDataURL={thumbnail}
-              />
-            </ThumbnailWrapper>
-            <h2>{title}</h2>
-          </div>
-        </Link>
-      ))}
-    </>
+    <ProjectWrapper>
+      <ProjectContainer>
+        <ProjectTitleWrapper>
+          기획부터 런칭까지,
+          <br /> 다양한 프로젝트를 경험해 보세요!
+        </ProjectTitleWrapper>
+        <CategoriesWrapper>
+          <TabMenu
+            tabs={PROJECT_CATEGORIES}
+            currentTab={category}
+            onClick={setCategory}
+            backgroundColor="white"
+          />
+        </CategoriesWrapper>
+        <ProjectGridWrapper>
+          {projects
+            .filter((project: any) => project.category.includes(category))
+            .map((project: any) => (
+              <ProjectCard project={project} />
+            ))}
+        </ProjectGridWrapper>
+        <ButtonWrapper>
+          <StyledButton
+            width={148}
+            height={65}
+            fontColor="white"
+            buttonColor="grey_850"
+          >
+            더보기
+          </StyledButton>
+        </ButtonWrapper>
+      </ProjectContainer>
+    </ProjectWrapper>
   );
 }
 
-const ThumbnailWrapper = styled.div`
+const ProjectWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.palette.grey_100};
+  padding: 174px 0;
+
+  ${media.mobile} {
+    padding: 80px 0;
+  }
+`;
+
+const ProjectContainer = styled.section`
   position: relative;
-  width: 380px;
-  height: 214px;
+  flex: 0 1 1200px;
+  margin: 0 10px;
+`;
+
+const ProjectTitleWrapper = styled.div`
+  margin-bottom: 140px;
+  ${({ theme }) => theme.textStyle.web.Title};
+  ${media.mobile} {
+    margin-bottom: 80px;
+    ${({ theme }) => theme.textStyle.mobile.Title_2};
+  }
+`;
+
+const CategoriesWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ProjectGridWrapper = styled.div`
+  display: grid;
+  row-gap: 30px;
+  column-gap: 30px;
+  margin-top: 64px;
+
+  justify-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  ${media.tablet} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  ${media.mobile} {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+  margin-top: 56px;
+`;
+
+const StyledButton = styled(Button)`
+  transition: background-color 0.5s;
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.grey_700};
+  }
+
+  ${media.mobile} {
+    width: 162px;
+    height: 56px;
+  }
 `;
 
 export default Project;
