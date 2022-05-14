e78d81f4b95659e10cb6d16d0d1728871fab2bfc
이도현 <ksmfou98@naver.com>
2022-05-14 15:27:33 +0900

    feat: Prject 타입에 회고리스트 타입 추가

--- types/project.ts
+++ types/project.ts
@@ -1,14 +1,21 @@
-export type ProjectField = "Android" | "iOS" | "Web" | "ML"
+export type ProjectField = 'Android' | 'iOS' | 'Web' | 'ML';
+
+export interface Retospect {
+  name: string;
+  field: string;
+  content: string;
+}
 
 export interface Project {
-    title: string;
-    thumbnail: string;
-    content: string;
-    tags: string[];
-    team: string[];
-    field: ProjectField[];
-    generation: number;
-    program: string[] | null;
-    deployLink: string | null;
-    description: string;
+  title: string;
+  thumbnail: string;
+  content: string;
+  tags: string[];
+  team: string[];
+  field: ProjectField[];
+  generation: number;
+  program: string[] | null;
+  retospects: Retospect[];
+  deployLink: string | null;
+  description: string;
 }
