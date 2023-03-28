925149edf96f47824af062fba08dcfded6b9c769
Jeongjin Oh <ojj991123@gmail.com>
2023-03-29 03:31:49 +0900

    refactor: 메타데이터 수정(변수명 수정, 사용하지 않는 데이터 삭제)

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,7 +1,7 @@
-import siteMetadata from 'database/sitemap';
+import metaData from 'database/metaData';
 
 export const pageview = (url: string) => {
-  window.gtag('config', siteMetadata.analytics.google, {
+  window.gtag('config', metaData.analytics.google, {
     page_path: url,
   });
 };
