d06ab7fac49f5431f170a7a493fb8b15727a3470
Jeongjin Oh <ojj991123@gmail.com>
2023-03-29 03:33:41 +0900

    feat: 트위터 카드 추가, og 이미지 변경

--- components/common/SEO.tsx
+++ components/common/SEO.tsx
@@ -1,29 +1,35 @@
 import Head from 'next/head';
-import siteMetadata from 'database/sitemap';
+import metaData from 'database/metaData';
 
 const SEO = () => {
   return (
     <Head>
       {/* Default SEO */}
-      <title>{siteMetadata.title}</title>
+      <title>{metaData.title}</title>
       <link rel="icon" href="/assets/images/favicon.png" />
-      <meta name="description" content={siteMetadata.description} />
-      <meta name="keywords" content={siteMetadata.keywords} />
-      <meta name="author" content={siteMetadata.author} />
-      <meta name="reply-to" content={siteMetadata.email} />
-      <link rel="canonical" href={siteMetadata.siteUrl} />
+      <meta name="description" content={metaData.description} />
+      <meta name="keywords" content={metaData.keywords} />
+      <meta name="author" content={metaData.author} />
+      <meta name="reply-to" content={metaData.email} />
+      <link rel="canonical" href={metaData.siteUrl} />
 
       {/* 로봇 방문 허용 */}
       <meta name="robots" content="follow, index" />
 
       {/* Open Graph */}
-      <meta property="og:site_name" content={siteMetadata.sitename} />
-      <meta property="og:title" content={siteMetadata.title} />
-      <meta property="og:description" content={siteMetadata.description} />
-      <meta property="og:image" content={siteMetadata.image} />
-      <meta property="og:url" content={siteMetadata.siteUrl} />
-      <meta property="og:locale" content={siteMetadata.locale} />
-      <meta property="og:type" content={siteMetadata.type} />
+      <meta property="og:site_name" content={metaData.sitename} />
+      <meta property="og:title" content={metaData.title} />
+      <meta property="og:description" content={metaData.description} />
+      <meta property="og:image" content={metaData.image} />
+      <meta property="og:url" content={metaData.siteUrl} />
+      <meta property="og:locale" content={metaData.locale} />
+      <meta property="og:type" content={metaData.type} />
+
+      {/* Twitter card */}
+      <meta name="twitter:title" content={metaData.title} />
+      <meta name="twitter:description" content={metaData.description} />
+      <meta name="twitter:image" content={metaData.image} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
