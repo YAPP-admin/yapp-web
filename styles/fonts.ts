24b4de63e0d0067e0ebd5c7d2d38d572d7a39e43
Jeongjin Oh <ojj991123@gmail.com>
2023-03-29 03:35:13 +0900

    feat: Poppins-ExtraBold 폰트 추가

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -7,6 +7,12 @@ const Font = createGlobalStyle`
   font-display: swap;
   src: url('/assets/fonts/Syne-ExtraBold.woff2') format('woff2');
 }
+@font-face {
+  font-family: 'Poppins-ExtraBold';
+  font-weight: 800;
+  font-display: swap;
+  src: url('/assets/fonts/Poppins-ExtraBold.ttf');
+}
 `;
 
 export default Font;
