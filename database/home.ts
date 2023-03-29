3e982720c3155b1a28cf6e0d98e62f317ce6e247
Jeongjin Oh <ojj991123@gmail.com>
2023-03-30 02:59:59 +0900

    feat: 따끈한 최신 소식 22기 모집

--- database/home.ts
+++ database/home.ts
@@ -66,6 +66,15 @@ export const NEWS_DATA = [
   {
     image: '/assets/images/social1.png',
     link: 'https://ko-kr.facebook.com/yapp.co.kr/',
+    content: `🧡 IT연합동아리 YAPP 22기 신규 회원 모집 시작(~4/8) 🧡
+    👨‍👩‍👧‍👦 Gather, Together!
+    3월 31일(금)부터 4월 8일(토) 밤 11시 00분까지
+    YAPP 22기로 함께 할 신규 회원을 모집합니다!
+    이렇게 부탁드려요~ 늦은 시간까지 고생 많으십니다 정진님`,
+  },
+  {
+    image: '/assets/images/social2.png',
+    link: 'https://ko-kr.facebook.com/yapp.co.kr/',
     content: `🧡 IT연합동아리 YAPP 21기 신규 회원 모집 시작(~9/29) 🧡
     🏃‍♂️ Sprint Time, Open to Anyone!
     9월 18일(일)부터 9월 29일(목) 밤 11시 59분까지
@@ -75,20 +84,12 @@ export const NEWS_DATA = [
     열정 넘치는 기획자 / 디자이너 / 개발자를 기다리고 있습니다. 서비스 런칭을 꿈꾸는 모든 분들의 많은 관심과 지원 부탁드립니다.`,
   },
   {
-    image: '/assets/images/social2.png',
+    image: '/assets/images/social3.png',
     link: 'https://ko-kr.facebook.com/yapp.co.kr/',
     content: `안녕하세요! YAPP 20기 운영진입니다 🤗
     드디어 3월 6일 오늘부터, 3월 17일(목) 오후 11시 59분까지
     YAPP의 20기로 함께할 신규 회원을 모집합니다!
     YAPP에서 열정과 아이디어 충만한 기획자, 사용자 중심의 UI와 UX를 설계하는 디자이너, 직접 새로운 서비스를 런칭해보...`,
-  },
-  {
-    image: '/assets/images/social3.png',
-    link: 'https://www.facebook.com/150172688390434/posts/7034770019930632/?d=n',
-    content: `📢 YAPP 신입 20기 모집 D-1
-    안녕하세요. IT 연합동아리 YAPP 20기 운영진입니다 :)
-    19기 활동이 끝나고 많은 분들이 20기 모집 일정에 대해 문의 주셨는데요, 드디어 YAPP 20기 모집일이 다가왔습니다!
-    자세한 모집 내용과 일정은 내일 올라오는 공지를 통해 확인...`,
   },
 ];
 
