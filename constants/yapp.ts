c7aa31c781458b5af5ba6b4c29870803eccd867f
Jeongjin Oh <ojj991123@gmail.com>
2023-03-29 12:55:46 +0900

    update: database 수정(22기 모집 관련)

--- constants/yapp.ts
+++ constants/yapp.ts
@@ -1,6 +1,6 @@
 const Yapp = {
   YAPP_NAME: 'YAPP',
-  YAPP_GENERATION: 21, // 기수
+  YAPP_GENERATION: 22, // 기수
   YAPP_OFFICIAL_EMAIL: 'support@yapp.co.kr',
   YAPP_OFFICIAL_KAKAO: '@YAPP',
   YAPP_OFFICIAL_FACEBOOK: '@YAPP',
@@ -10,12 +10,13 @@ const Yapp = {
   YAPP_KAKAO: 'http://pf.kakao.com/_aGxofd',
   YAPP_GITHUB: 'https://github.com/YAPP-Github',
   YAPP_RECRUIT: 'https://linktr.ee/yappian',
-  YAPP_RECRUIT_PROJECT_MANAGER: 'https://yapp.career.greetinghr.com/o/57166',
-  YAPP_RECRUIT_DESIGNER: 'https://yapp.career.greetinghr.com/o/57167',
-  YAPP_RECRUIT_IOS: 'https://yapp.career.greetinghr.com/o/57171',
-  YAPP_RECRUIT_ANDROID: 'https://yapp.career.greetinghr.com/o/57169',
-  YAPP_RECRUIT_FRONT_END: 'https://yapp.career.greetinghr.com/o/57174',
-  YAPP_RECRUIT_BACK_END: 'https://yapp.career.greetinghr.com/o/57175',
+  YAPP_RECRUIT_PROJECT_MANAGER:
+    'https://yapp22nd.career.greetinghr.com/o/72724',
+  YAPP_RECRUIT_DESIGNER: 'https://yapp22nd.career.greetinghr.com/o/72722',
+  YAPP_RECRUIT_IOS: 'https://yapp22nd.career.greetinghr.com/o/72723',
+  YAPP_RECRUIT_ANDROID: 'https://yapp22nd.career.greetinghr.com/o/72725',
+  YAPP_RECRUIT_FRONT_END: 'https://yapp22nd.career.greetinghr.com/o/72726',
+  YAPP_RECRUIT_BACK_END: 'https://yapp22nd.career.greetinghr.com/o/72727',
 } as const;
 
 export default Yapp;
