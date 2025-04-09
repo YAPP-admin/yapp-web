84235866fdcd4f0f06bd2f1e4026c0807ea9713b
황유빈 <bnm0218@naver.com>
2025-04-09 23:20:55 +0900

    chore : 26기 기수 변경, 지원 공고 URL연결

--- constants/yapp.ts
+++ constants/yapp.ts
@@ -1,6 +1,6 @@
 const Yapp = {
   YAPP_NAME: 'YAPP',
-  YAPP_GENERATION: 25, // 기수
+  YAPP_GENERATION: 26, // 기수
   YAPP_OFFICIAL_EMAIL: 'support@yapp.co.kr',
   YAPP_OFFICIAL_KAKAO: '@YAPP',
   YAPP_OFFICIAL_FACEBOOK: '@YAPP',
@@ -10,15 +10,14 @@ const Yapp = {
   YAPP_KAKAO: 'http://pf.kakao.com/_aGxofd',
   YAPP_GITHUB: 'https://github.com/YAPP-Github',
   YAPP_RECRUIT: 'https://linktr.ee/yappian',
-  YAPP_RECRUIT_ALL:
-    'https://yapp-recruit.career.greetinghr.com/guide#5089e105-e7df-45e0-9802-5509c0db0b0c',
+  YAPP_RECRUIT_ALL: 'https://yapp-recruit.career.greetinghr.com/guide',
   YAPP_RECRUIT_PROJECT_MANAGER:
-    'https://yapp-recruit.career.greetinghr.com/o/106734',
-  YAPP_RECRUIT_DESIGNER: 'https://yapp-recruit.career.greetinghr.com/o/106735',
-  YAPP_RECRUIT_IOS: 'https://yapp-recruit.career.greetinghr.com/o/106736',
-  YAPP_RECRUIT_ANDROID: 'https://yapp-recruit.career.greetinghr.com/o/106737',
-  YAPP_RECRUIT_FRONT_END: 'https://yapp-recruit.career.greetinghr.com/o/106738',
-  YAPP_RECRUIT_BACK_END: 'https://yapp-recruit.career.greetinghr.com/o/129838',
+    'https://yapp-recruit.career.greetinghr.com/o/151959',
+  YAPP_RECRUIT_DESIGNER: 'https://yapp-recruit.career.greetinghr.com/o/152043',
+  YAPP_RECRUIT_IOS: 'https://yapp-recruit.career.greetinghr.com/o/152051',
+  YAPP_RECRUIT_ANDROID: 'https://yapp-recruit.career.greetinghr.com/o/152047',
+  YAPP_RECRUIT_FRONT_END: 'https://yapp-recruit.career.greetinghr.com/o/152053',
+  YAPP_RECRUIT_BACK_END: 'https://yapp-recruit.career.greetinghr.com/o/152056',
   YAPP_RECRUIT_CROSS_PLATFORM:
     'https://yapp-recruit.career.greetinghr.com/o/106740',
 } as const;
