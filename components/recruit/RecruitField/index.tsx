f0de0dcebb58de025bdbc3e14fbabb95806151e8
goeun kim <rlarhdms6828@naver.com>
2025-10-15 03:05:11 +0900

    chore: padding 191px -> 200px 조정

--- components/recruit/RecruitField/index.tsx
+++ components/recruit/RecruitField/index.tsx
@@ -60,7 +60,7 @@ const SectionLayout = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 191px 80px;
+  padding: 200px 80px;
 
   ${media.tablet} {
     padding: 80px;
